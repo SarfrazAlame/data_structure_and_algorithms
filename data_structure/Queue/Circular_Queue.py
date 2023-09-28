5006081cee1f0ab50c4dace0c3f5d9a5119568ce
Sarfraz Alam <sarfrazk7858@gmail.com>
2023-09-28 10:17:43 +0530

    Queue

--- data_structure/Queue/Circular_Queue.py
+++ data_structure/Queue/Circular_Queue.py
@@ -1,5 +1,84 @@
+# class Queue:
+#     def __init__(self, maxSize):
+#         self.items = maxSize * [None]
+#         self.maxSize = maxSize
+#         self.start = -1
+#         self.top = -1
+
+#     def __str__(self):
+#         values = [str(x) for x in self.items]
+#         return " ".join(values)
+
+#     def isFull(self):
+#         if self.top + 1 == self.start:
+#             return True
+#         elif self.start == 0 and self.top + 1 == self.maxSize:
+#             return True
+#         else:
+#             return False
+
+#     def isEmpty(self):
+#         if self.top == -1:
+#             return True
+#         else:
+#             return False
+
+#     def enqueue(self,value):
+#         if self.isFull():
+#             return "Queue is full"
+#         else:
+#             if self.top+1 ==self.maxSize:
+#                 self.top = 0
+#             else:
+#                 self.top+=1
+#                 if self.start==-1:
+#                     self.start =0
+#             self.items[self.top] = value
+#             return "the element is inserted at the end of the Queue"
+
+#     def dequeue(self):
+#         if self.isEmpty():
+#             return "There is not any element in the queue"
+#         else:
+#             firstElement = self.items[self.start]
+#             start = self.start
+#             if self.start==self.top:
+#                 self.start=-1
+#                 self.top = -1
+#             elif self.start+1 == self.maxSize:
+#                 self.start=0
+#             else:
+#                 self.start+=1
+#             self.items[start]=None
+#             return firstElement
+
+#     def peek(self):
+#         if self.isEmpty():
+#             return "there is not any element to peek"
+#         else:
+#             return self.items[self.start]
+
+#     def delete(self):
+#         self.items = self.maxSize*[None]
+#         self.top=-1
+#         self.start=-1
+
+
+# customQueue = Queue(3)
+# # print(customQueue.isEmpty())
+# customQueue.enqueue(10)
+# customQueue.enqueue(20)
+# customQueue.enqueue(30)
+# # print(customQueue.peek())
+# customQueue.delete()
+# print(customQueue)
+
+
+##################################   Practice Circular Queue ##################
+
+
 class Queue:
-    def __init__(self,maxSize):
+    def __init__(self, maxSize):
         self.items = maxSize * [None]
         self.maxSize = maxSize
         self.start = -1
@@ -7,20 +86,41 @@ class Queue:
 
     def __str__(self):
         values = [str(x) for x in self.items]
-        return ' '.join(values)
-    
+        return " ".join(values)
+
     def isFull(self):
-        if self.top+1 == self.start:
+        if self.top + 1 == self.start:
             return True
-        elif self.start+1== 0 and self.top+1==self.maxSize:
-            return  True
+        elif self.start == 0 and self.top + 1 == self.maxSize:
+            return True
         else:
             return False
-    
+
     def isEmpty(self):
-        if self.top==-1:
+        if self.top == -1:
             return True
         else:
             return False
-        
- 
+
+    def enqueue(self, value):
+        if self.isFull():
+            return "Queue is full"
+        else:
+            if self.top + 1 == self.maxSize:
+                self.top = 0
+            else:
+                self.top += 1
+                if self.start == -1:
+                    self.start = 0
+            self.items[self.top] = value
+            return "element inserted in the end of queue"
+    
+
+
+customQueue = Queue(3)
+# print(customQueue.isEmpty())
+# print(customQueue.isFull())
+customQueue.enqueue(10)
+customQueue.enqueue(20)
+customQueue.enqueue(30)
+print(customQueue)
